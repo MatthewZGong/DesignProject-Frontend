2aad6ce57aed7ac639413258f8a057e5a90be14f
koswjjnd <sj3192@nyu.edu>
2024-02-12 23:29:40 -0500

    add home page

--- src/App.js
+++ src/App.js
@@ -11,11 +11,13 @@ import Navbar from './Components/Navbar';
 import Jobs from './Components/Jobs';
 import Login from './Components/Login';
 import CreateAccount from './Components/CreateAccount';
+import Home from './Components/Home';
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="jobs" element={<Jobs />} />
         <Route path="login" element={<Login />} />
         <Route path="createAccount" element={<CreateAccount />} />

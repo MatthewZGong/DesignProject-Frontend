fe8ab60ad4bdbf101bb4ca44601fe5e0dc3e02b7
koswjjnd <sj3192@nyu.edu>
2024-02-12 20:32:28 -0500

    add login page

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Send the data to the backend for authenticaton
+    console.log({ username, password });
+    // Redirect the user to another page, etc.
+  };
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;

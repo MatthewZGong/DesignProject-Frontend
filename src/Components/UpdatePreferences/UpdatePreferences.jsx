42e9fb6f5d7920daa0e0fb32a9d533586e1a1817
MichelleLin5 <ml7188@nyu.edu>
2024-03-10 18:50:29 -0400

    deleted mistake function in preference form

--- src/Components/UpdatePreferences/UpdatePreferences.jsx
+++ src/Components/UpdatePreferences/UpdatePreferences.jsx
@@ -12,35 +12,5 @@ function jobsByPreferences() {
     );
   }
   
-// function MyForm() {
-//     return (
-//       <>
-//         <label>
-//           Text input: <input name="myInhiput" />
-//         </label>
-//         <hr />
-//         <label>
-//           Checkbox: <input type="checkbox" name="myCheckbox" />
-//         </label>
-//         <hr />
-//         <p>
-//           Radio buttons:
-//           <label>
-//             <input type="radio" name="myRadio" value="option1" />
-//             Option 1
-//           </label>
-//           <label>
-//             <input type="radio" name="myRadio" value="option2" />
-//             Option 2
-//           </label>
-//           <label>
-//             <input type="radio" name="myRadio" value="option3" />
-//             Option 3
-//           </label>
-//         </p>
-//       </>
-//     );
-//   }
-  
 
 export default jobsByPreferences;

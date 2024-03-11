3cdecba8bef4a17795aa6cc51865207259acfefb
koswjjnd <sj3192@nyu.edu>
2024-03-10 23:52:33 -0400

    update UpdatePreferences webpage

--- src/Components/UpdatePreferences/UpdatePreferences.jsx
+++ src/Components/UpdatePreferences/UpdatePreferences.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function jobsByPreferences() {
-    return (
+function JobsByPreferences() {
+  const [numDays, setNumDays] = useState('');
+
+  const handleShowJobs = () => {
+    console.log(`Showing jobs from the most recent ${numDays} days`);
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>View Jobs by Preferences</h1>
+      </header>
       <div>
-        <header>
-          <h1>View Jobs by Preferences</h1>
-          <button type="button" onClick={true}>Update User Preferences</button>
-        </header>
-        <button onClick={() => window.location.href = '/updatePreferencesForm'}>Update Preferences</button>
+        
+        <label>
+          Show Jobs from the most recent
+          <input
+            style={{ margin: '0 5px' }} 
+            type="number"
+            value={numDays}
+            onChange={(e) => setNumDays(e.target.value)}
+            placeholder="Number"
+            min="1"
+          />
+          Days
+        </label>
+        <button onClick={handleShowJobs}>Enter</button>
       </div>
-    );
-  }
-  
+      <button onClick={() => window.location.href = '/updatePreferencesForm'}>Update Preferences</button>
+    </div>
+  );
+}
 
-export default jobsByPreferences;
+export default JobsByPreferences;

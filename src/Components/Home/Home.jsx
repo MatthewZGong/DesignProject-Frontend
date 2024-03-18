40cf6af09da37e6df694d9a971e64c56c93a44fc
TanzHasan <tanzirhasanhasan@gmail.com>
2024-03-17 21:09:02 -0400

    Added more material to homepage

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+// make Home page with links to other pages
 
 function Home() {
   return (
     <div>
-      <h1>Welcome to the Job Portal</h1>
+      <h1>Welcome to the Job Board</h1>
+
+      <p>Please update your user information</p>
+      <p>Update your user preferences</p>
+      <p>View all jobs</p>
+      <p>Delete your account</p>
     </div>
   );
 }

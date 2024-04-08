659b70512c2ca176054684a0d8d4bf98455f8605
MichelleLin5 <ml7188@nyu.edu>
2024-04-07 20:39:07 -0400

    photo for home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAuth } from '../../AuthContext';
 import { useNavigate } from 'react-router-dom';
+import logo from './logo.jpeg';
 
 function Home() {
   const { isLoggedIn } = useAuth();
@@ -13,8 +14,10 @@ function Home() {
   return (
     <div>
       <h1>Welcome to the Job Board</h1>
+      <img src={logo} alt="Logo" />
       {isLoggedIn && (
         <button onClick={handleViewProfile}>View your profile</button>
+        
       )}
     </div>
   );

44ec2e94eb2236fa79c774b3ecf3e047e7571abc
MichelleLin5 <ml7188@nyu.edu>
2024-04-07 21:02:27 -0400

    homepage

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -15,6 +15,8 @@ function Home() {
     <div>
       <h1>Welcome to the Job Board</h1>
       <img src={logo} alt="Logo" />
+      <br />
+      <button onClick={() => window.location.href = '/createAccount'}>Create Account and Explore Jobs</button>
       {isLoggedIn && (
         <button onClick={handleViewProfile}>View your profile</button>
         

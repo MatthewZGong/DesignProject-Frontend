ca2825109fd57899739c1983d7af0c980b3615ff
TanzHasan <tanzirhasanhasan@gmail.com>
2024-04-09 10:50:51 -0400

    Prevent making new accounts while logged in

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -16,10 +16,11 @@ function Home() {
       <h1>Welcome to the Job Board</h1>
       <img src={logo} alt="Logo" />
       <br />
-      <button onClick={() => window.location.href = '/createAccount'}>Create Account and Explore Jobs</button>
+      {!isLoggedIn && (
+        <button onClick={() => window.location.href = '/createAccount'}>Create Account and Explore Jobs</button>
+      )}
       {isLoggedIn && (
         <button onClick={handleViewProfile}>View your profile</button>
-        
       )}
     </div>
   );
